137a321d56194da413bf639261a27a59969c324a
rkemmy <rkemmy.rk69@gmail.com>
2018-09-18 18:33:32 +0300

    [#160537190] Register orders blueprint

---
+++ app/__init.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_restful import Api
+from app.api_v1.orders.views import Orders
+from instance.config import app_config
+
+
+
+def create_app(config_stage):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_stage])
+    app.config.from_pyfile('config.py')
+
+    
+    from .api_v1.orders import orders_bp as orders_blueprint
+    api = Api(orders_blueprint)
+    app.register_blueprint(orders_blueprint, url_prefix='/api/v1')
+
+    api.add_resource(Orders, '/orders')
+    
+
+
+
+    return app
