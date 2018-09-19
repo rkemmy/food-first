a3e2140a443a4d480e3fa75771aa804aec6b26f0
rkemmy <rkemmy.rk69@gmail.com>
2018-09-19 18:47:26 +0300

    [#160605330] iclude instance

---
+++ instance/config.py
@@ -0,0 +1,26 @@
+import os
+
+class Config(object):
+    """ Parent configuration class """
+    pass
+    
+
+class DevelopmentConfig(Config):
+    """ Development configuration class that inherits from the Config Parent Class"""
+    DEBUG = True
+
+class TestConfig(Config):
+    """ Testing configuration class that inherits from the Config Parent Class"""
+    DEBUG = True
+    TESTING = True
+
+class ProductionConfig(Config):
+    """ Prduction configuration class that inherits from the Config Parent Class"""
+    DEBUG = False
+    TESTING = False
+
+app_config = {
+    'development' : DevelopmentConfig,
+    'testing' : TestConfig,
+    'production' : ProductionConfig
+}
