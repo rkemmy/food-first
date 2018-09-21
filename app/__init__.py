d69bd82f2d1e4e9b125e8551c228cd537f3daa01
rkemmy <rkemmy.rk69@gmail.com>
2018-09-21 23:40:48 +0300

    [#160605330] Add order resource urls

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_restful import Api
-from app.api_v1.orders.views import Orders, GetOneOrder
+from app.api_v1.orders.views import Orders, GetOneOrder,AcceptOrder,ApprovedOrders,DeclineOrder,DeclinedOrders,PendingOrders,CompleteOrder,CompletedOrders
 from instance.config import app_config
 
 
@@ -17,7 +17,14 @@ def create_app(config_stage):
 
     api.add_resource(Orders, '/orders')
     api.add_resource(GetOneOrder, '/orders/<int:id>')
-    
+    api.add_resource(AcceptOrder,'/orders/accept/<int:id>')
+    api.add_resource(ApprovedOrders,'/orders/approved')
+    api.add_resource(DeclineOrder,'/orders/decline/<int:id>')
+    api.add_resource(DeclinedOrders,'/orders/declined')
+    api.add_resource(PendingOrders,'/orders/pending')
+    api.add_resource(CompleteOrder,'/orders/completeorder/<int:id>')
+    api.add_resource(CompletedOrders,'/orders/completed')
+
 
 
 
