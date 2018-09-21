86fcd5499738d1380742377544935ab5ef5acaec
rkemmy <rkemmy.rk69@gmail.com>
2018-09-21 16:25:26 +0300

    [#160605330] Restructure put method

--- app/api_v1/orders/views.py
+++ app/api_v1/orders/views.py
@@ -6,12 +6,14 @@ from app.models import orders, Order
 class Orders(Resource):
 
     def get(self):
+        ''' Method that get all orders '''
         if len(orders) == 0:
             return {"message":"empty list"}, 200
         else:
             return {"Orders": [order.serialize() for order in orders]}, 200
 
     def post(self):
+        ''' Method that posts an order '''
         data = request.get_json(force=True)
         if not data:
             return ({'message': 'No input data provided'}, 400)
@@ -42,6 +44,7 @@ class Orders(Resource):
 class GetOneOrder(Resource):
     
     def get(self, id):
+        ''' Method that gets a spedific order by id '''
         
         order  = Order().get_by_id(id)
         if not order:
@@ -51,28 +54,21 @@ class GetOneOrder(Resource):
         
 
     def put(self, id):
-        data = request.get_json(force=True)
-        for item in orders:
-            if item['id']== id:
-                item['name'] = data['name']
-                item['description'] = data['description'] 
-                item['price'] = data['price']
-                # item['status'] = data['status']
-                return item, 200 
+        ''' Method that updates a specific order '''
         
-        item = {
-            "id" : id,
-            "name": data["name"],
-            "description" : data["description"],
-            "price" : data["price"],
-            # "status" : data["status"]
-        }
-
-        orders.append(item)
-        return item, 201
+        order = Order().get_by_id(id)
+        if order:
+            data = request.get_json()
+            order.name= data['name']
+            order.description = data['description']
+            order.price = data['price']
+            return {"message":order.serialize()},201
+        
+        return {"Message":"Order not found"},404
 
 
     def delete(self, id):
+        ''' Method that deletes a specific id '''
         order  = Order().get_by_id(id)
         if not order:
             return {"Message":"Order not found"},404
