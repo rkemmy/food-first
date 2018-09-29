793c2f321e7d97b8f4320c96233293c73a7ab10c
rkemmy <rkemmy.rk69@gmail.com>
2018-09-29 23:17:54 +0300

    [#160621909] generate coverage report

--- instance/config.py
+++ instance/config.py
@@ -3,16 +3,19 @@ import os
 class Config(object):
     """ Parent configuration class """
     pass
-    
+    CSRF_ENABLED = True
+    SECRET = os.getenv('SECRET')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 
 class DevelopmentConfig(Config):
     """ Development configuration class that inherits from the Config Parent Class"""
     DEBUG = True
-
+    
 class TestConfig(Config):
     """ Testing configuration class that inherits from the Config Parent Class"""
     DEBUG = True
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_TEST_URL')
 
 class ProductionConfig(Config):
     """ Prduction configuration class that inherits from the Config Parent Class"""
