8ffd5398082f4b603fbc91e82a0f7f91126dd341
rkemmy <rkemmy.rk69@gmail.com>
2018-10-01 06:31:47 +0300

    [#160812131] Connect to the databse and create tables

--- instance/config.py
+++ instance/config.py
@@ -3,12 +3,12 @@ import os
 class Config(object):
     """ Parent configuration class """
     pass
+    CSRF_ENABLED = True
     
-
 class DevelopmentConfig(Config):
     """ Development configuration class that inherits from the Config Parent Class"""
     DEBUG = True
-
+    
 class TestConfig(Config):
     """ Testing configuration class that inherits from the Config Parent Class"""
     DEBUG = True
