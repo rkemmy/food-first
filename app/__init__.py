6885c7ac541d8fbf487c0b4948e2baf8257e9f1b
rkemmy <rkemmy.rk69@gmail.com>
2018-10-01 06:52:43 +0300

    [#160812156] Register auth blueprint and add it as resource

--- app/__init__.py
+++ app/__init__.py
@@ -2,17 +2,22 @@ from flask import Flask
 from flask_restful import Api
 from app.api.v1.views import Orders, GetOneOrder,AcceptOrder,DeclineOrder,CompleteOrder,Status
 from instance.config import app_config
-
-
+from app.api.v2.auth import Signup
 
 def create_app(config_stage):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_stage])
 
+    from .api.v2.auth import auth_blueprint as auth_bp
+    auth = Api(auth_bp)
+    app.register_blueprint(auth_bp, url_prefix='/api/v2/auth')
+
+
     from .api.v1 import orders_bp as orders_blueprint
     api = Api(orders_blueprint)
     app.register_blueprint(orders_blueprint, url_prefix='/api/v1')
 
+    auth.add_resource(Signup, '/signup')
     api.add_resource(Orders, '/orders')
     api.add_resource(GetOneOrder, '/orders/<int:id>')
     api.add_resource(AcceptOrder,'/orders/accept/<int:id>')
