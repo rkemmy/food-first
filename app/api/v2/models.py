ecd41212b00cc16f3d42faa4f891c4dc04e7ea68
rkemmy <rkemmy.rk69@gmail.com>
2018-10-02 10:34:58 +0300

    [#160812191] Create order model and method to get meal by name

--- app/api/v2/models.py
+++ app/api/v2/models.py
@@ -91,7 +91,7 @@ class MealItem(ConnectDB):
         self.price = price
 
     def add(self):
-        ''' Add food item to fooditems table'''
+        ''' Add meal item to meals table'''
         self.cursor.execute(
             ''' INSERT INTO meals(name, description, price) VALUES(%s, %s, %s)''',
             (self.name, self.description, self.price))
@@ -99,8 +99,22 @@ class MealItem(ConnectDB):
         self.connection.commit()
         self.cursor.close()
 
+    def get_meal_by_name(self, name):
+        '''fetch an item by id'''
+        self.cursor.execute(""" SELECT * FROM meals WHERE name = '%s' """ %
+                            (name))
+
+        meal = self.cursor.fetchone()
+
+        self.connection.commit()
+        self.cursor.close()
+
+        if meal:
+            return self.objectify(meal)
+        return None
+
     def get_all_meals(self):
-        '''  Get all food meals '''
+        '''  Get all meal items '''
         self.cursor.execute(''' SELECT * FROM meals''')
 
         meals = self.cursor.fetchall()
@@ -127,4 +141,66 @@ class MealItem(ConnectDB):
         item.id = data[0]
         self = item
         return self
-          
+        
+class Order(ConnectDB):
+    def __init__(self,
+                 username=None,
+                 title=None,
+                 description = None,
+                 price=None,
+                 status="Pending"):
+        super().__init__()
+        self.username = username
+        self.title = title
+        self. description = description
+        self.price = price
+        self.status = status
+        self.date = datetime.now()
+
+    def add(self):
+        ''' Add food order to database'''
+        self.cursor.execute(
+            ''' INSERT INTO orders(username, title, description, price, status, date) VALUES(%s, %s, %s, %s,%s, %s, %s)''',
+            (self.username, self.title, self.description, self.price,
+             self.status, self.date))
+
+        self.connection.commit()
+        self.cursor.close()
+
+    def get_by_id(self, order_id):
+        '''fetch an order by id'''
+        self.cursor.execute(''' SELECT * FROM orders WHERE id=%s''',
+                            (order_id, ))
+
+        order = self.cursor.fetchone()
+
+        self.connection.commit()
+        self.cursor.close()
+
+        if order:
+            return self.objectify(order)
+        return None
+
+    def serialize(self):
+        ''' return object as a dictionary '''
+        return dict (
+            id = self.id,
+            title = self.title,
+            description = self.description,
+            price = self.price,
+            status = self.status,
+            date = self.date
+        )
+    
+    def objectify(self, data):
+        ''' map tuple to an object '''
+        order = Order(
+            username=data[1],
+            description=data[2],
+            title=data[3],
+            price=data[4],
+            status=data[5])
+        order.id = data[0]
+        order.date = str(data[6])
+        self = order
+        return self
