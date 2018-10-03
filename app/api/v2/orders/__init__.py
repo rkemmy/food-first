3f02cd3592db8fd7c91311c1adfec02ae26be2fb
rkemmy <rkemmy.rk69@gmail.com>
2018-10-03 11:38:00 +0300

    [#160812316] Create user history blueprint

--- app/api/v2/orders/__init__.py
+++ app/api/v2/orders/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-from .orders import PostOrder, SpecificOrder
+from .orders import PostOrder, SpecificOrder, UserHistory
 
 orderly_blueprint = Blueprint('orderly', __name__)
